5cb80b9f5a04e0572c155b64fa9de8ba20190c29
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-08-23 13:31:48 -0700

    Cleanup and add Solar Voltage Input

--- Source/GnssTracker/GnssTrackerHardwareV1.cs
+++ Source/GnssTracker/GnssTrackerHardwareV1.cs
@@ -11,21 +11,57 @@ using System;
 
 namespace WildernessLabs.Hardware.GnssTracker
 {
+    /// <summary>
+    /// Represents a Gnss Tracker Hardware V1
+    /// </summary>
     public class GnssTrackerHardwareV1 : IGnssTrackerHardware
     {
         protected Logger Log = Resolver.Log;
-        protected IF7CoreComputeMeadowDevice Device { get; }
 
-        public II2cBus I2cBus { get; }
-        public ISpiBus SpiBus { get; }
-        public PwmLed OnboardLed { get; protected set; }
-        public Bme688 AtmosphericSensor { get; protected set; }
-        public NeoM8 Gnss { get; protected set; }
-        public IGraphicsDisplay Display { get; protected set; }
+        //protected IF7CoreComputeMeadowDevice Device { get; }
 
+        /// <summary>
+        /// Gets the I2C Bus
+        /// </summary>
+        public II2cBus? I2cBus { get; protected set; }
+
+        /// <summary>
+        /// Gets the SPI Bus
+        /// </summary>
+        public ISpiBus? SpiBus { get; protected set; }
+
+        /// <summary>
+        /// Gets the PWM LED
+        /// </summary>
+        public PwmLed? OnboardLed { get; protected set; }
+
+        /// <summary>
+        /// Gets the BME688 atmospheric sensor
+        /// </summary>
+        public Bme688? AtmosphericSensor { get; protected set; }
+
+        /// <summary>
+        /// The Neo GNSS sensor
+        /// </summary>
+        public NeoM8? Gnss { get; protected set; }
+
+        /// <summary>
+        /// Gets the e-paper display
+        /// </summary>
+        public IGraphicsDisplay? Display { get; protected set; }
+
+        /// <summary>
+        /// Gets the Solar Voltage Input
+        /// </summary>
+        public IAnalogInputPort? SolarVoltageInput { get; protected set; }
+
+        /// <summary>
+        /// Create a new GnssTrackerHardwareV1 object
+        /// </summary>
+        /// <param name="device"></param>
         public GnssTrackerHardwareV1(IF7CoreComputeMeadowDevice device)
         {
-            Device = device;
+            //Device = device;
 
             Log.Debug("Initialize hardware...");
 
@@ -33,7 +69,7 @@ namespace WildernessLabs.Hardware.GnssTracker
             {
                 Log.Debug("Initializing Onboard LED");
 
-                OnboardLed = new PwmLed(Device.Pins.D20, TypicalForwardVoltage.Green);
+                OnboardLed = new PwmLed(device.Pins.D20, TypicalForwardVoltage.Green);
 
                 Log.Debug("Onboard LED initialized");
             }
@@ -46,7 +82,7 @@ namespace WildernessLabs.Hardware.GnssTracker
             {
                 Log.Debug("Initializing BME688");
 
-                I2cBus = Device.CreateI2cBus();
+                I2cBus = device.CreateI2cBus();
                 AtmosphericSensor = new Bme688(I2cBus, (byte)Bme688.Addresses.Address_0x76);
 
                 Log.Debug("BME688 initialized");
@@ -60,7 +96,7 @@ namespace WildernessLabs.Hardware.GnssTracker
             {
                 Resolver.Log.Debug("Initializing GNSS");
 
-                Gnss = new NeoM8(Device, Device.PlatformOS.GetSerialPortName("COM4"), Device.Pins.D09, Device.Pins.D11);
+                Gnss = new NeoM8(device, device.PlatformOS.GetSerialPortName("COM4"), device.Pins.D09, device.Pins.D11);
 
                 Resolver.Log.Debug("GNSS initialized");
             }
@@ -74,17 +110,17 @@ namespace WildernessLabs.Hardware.GnssTracker
                 Resolver.Log.Debug("Initializing ePaper Display");
 
                 var config = new SpiClockConfiguration(new Frequency(48000, Frequency.UnitType.Kilohertz), SpiClockConfiguration.Mode.Mode0);
-                SpiBus = Device.CreateSpiBus(
-                    Device.Pins.SCK,
-                    Device.Pins.COPI,
-                    Device.Pins.CIPO,
+                SpiBus = device.CreateSpiBus(
+                    device.Pins.SCK,
+                    device.Pins.COPI,
+                    device.Pins.CIPO,
                     config);
                 Display = new Ssd1680(
                     spiBus: SpiBus,
-                    chipSelectPin: Device.Pins.D02,
-                    dcPin: Device.Pins.D03,
-                    resetPin: Device.Pins.D04,
-                    busyPin: Device.Pins.D05,
+                    chipSelectPin: device.Pins.D02,
+                    dcPin: device.Pins.D03,
+                    resetPin: device.Pins.D04,
+                    busyPin: device.Pins.D05,
                     width: 122,
                     height: 250);
 
@@ -93,6 +129,17 @@ namespace WildernessLabs.Hardware.GnssTracker
             catch (Exception e)
             {
                 Resolver.Log.Error($"Err initializing ePaper Display: {e.Message}");
+            }
+
+            try
+            {
+                Resolver.Log.Debug("Instantiating Solar Voltage Input");
+                SolarVoltageInput = device.Pins.A02.CreateAnalogInputPort(5);
+                Resolver.Log.Debug("Solar Voltage Input up");
+            }
+            catch (Exception ex)
+            {
+                Resolver.Log.Error($"Unabled to create the Switching Anemometer: {ex.Message}");
             }
         }
     }
