60de9472fa9d394eb34428dde83703c12e4dcb91
Jorge Ramirez <ramirez.jorgea@outlook.com>
2024-04-22 21:11:13 -0700

    Add CO2 bug repro sample

---
+++ Source/Additional Samples/CO2_Bug/MeadowApp.cs
@@ -0,0 +1,38 @@
+﻿using Meadow;
+using Meadow.Devices;
+using System;
+using System.Threading.Tasks;
+
+namespace CO2_Bug;
+
+public class MeadowApp : App<F7CoreComputeV2>
+{
+    IGnssTrackerHardware hardware;
+
+    public override Task Initialize()
+    {
+        Resolver.Log.Info("Initialize...");
+
+        hardware = GnssTracker.Create();
+
+        return Task.CompletedTask;
+    }
+
+    public override async Task Run()
+    {
+        Resolver.Log.Info("Run...");
+
+        while (true)
+        {
+            Resolver.Log.Info("Hello, Meadow Core-Compute!");
+
+            if (hardware.CO2ConcentrationSensor is { } co2Sensor)
+            {
+                var co2 = await co2Sensor.Read();
+                Resolver.Log.Info($"co2 = {co2.PartsPerMillion}");
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(5));
+        }
+    }
+}
